1878df17d2feb8b9988f3a5e70a42ace1b96eeaf
in5248 <in5248@naver.com>
2025-06-15 00:48:52 +0900

    fix: simplify middleware to fix edge function error

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/posts(.*)", "/api/comments(.*)", "/categories(.*)"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: [
-    "/((?!api|trpc))(_next|.+\\..+)(.*)"],
-});
+export function middleware(request: NextRequest) {
+  // 모든 요청 허용
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!api|trpc|_next|.*\\..*).*)"],
 };
